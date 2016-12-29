a01873187a4d16324ecc9792fe5f507e8b38521a
Eri Mehmeti <contact@eridesigns.com.au>
2016-12-29 12:06:10 -0500

    imporved layout and added menu slide

--- assets/js/production/custom.js
+++ assets/js/production/custom.js
@@ -21,6 +21,26 @@ $(document).ready(function() {
     contentType: 'html'
   });
 
+  //menu panel
+  $('.menu-panel-slide').click(function(){
+      $('.menu-panel').slideToggle();
+  });
+
+  // Defining a function to set size for #hero
+  function fullscreen(){
+    $(".hero").css({
+      width: $(window).width(),
+      height: $(window).height()
+      });
+  }
+
+  fullscreen();
+
+  // Run the function in case of window resize
+  $(window).resize(function() {
+       fullscreen();
+  });
+
 });
 
 })(jQuery); // end
